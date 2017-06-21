623ee5afbfd9d337accb9f2450aa828a5004c3f1
Slawek Plamowski <slawek.plamowski@gmail.com>
2017-06-21 05:59:33 +0200

    Samsung camera intent problem

--- app/src/main/java/com/slq/tokfm/MainActivity.kt
+++ app/src/main/java/com/slq/tokfm/MainActivity.kt
@@ -9,6 +9,9 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import android.R.attr.data
+import android.provider.MediaStore
+import android.support.v4.app.NotificationCompat.getExtras
 
 
 class MainActivity : AppCompatActivity() {
@@ -26,23 +29,18 @@ class MainActivity : AppCompatActivity() {
 
     fun listPodcasts(view: View) {
         Log.d("LIST PODCASTS", "listPodcasts called!")
-        val intent = Intent()
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         intent.type = "image/*"
         intent.action = Intent.ACTION_GET_CONTENT
         intent.addCategory(Intent.CATEGORY_OPENABLE)
-        startActivityForResult(intent, REQUEST_CODE)
+        startActivityForResult(Intent.createChooser(intent,"Select Picture"), REQUEST_CODE)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        when {
-            requestCode != REQUEST_CODE -> return
-            resultCode != Activity.RESULT_OK -> return
-            else -> {
-                bitmap?.recycle();
-                val stream = getContentResolver().openInputStream(data?.getData())
-                bitmap = BitmapFactory.decodeStream(stream)
-                imageView?.setImageBitmap(bitmap);
-            }
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            val extras = data?.getExtras()
+            val imageBitmap = extras?.get("data")
+            imageView?.setImageBitmap(imageBitmap as Bitmap?)
         }
     }
 }
